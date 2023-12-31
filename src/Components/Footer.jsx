f2bed07abcd5f09748d9ebef6a412361368a6e71
VictorIdowu <idsonvictor@gmail.com>
2023-12-31 20:12:14 +0100

    Some more

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,15 +2,19 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Footer = (props) => {
-  const [active, setActive] = useState(false);
+  const [active, setActive] = useState("");
   const currentYear = new Date().getFullYear();
 
   return (
     <footer className={`footer ease-in duration-500 bg-basketball`}>
       <ul className="flex text-base sm:text-xl gap-10">
         {["About", "Contact", "Projects"].map((item, i) => (
-          <li key={i + 1} className="nav">
-            <Link to={`/${item.toLowerCase()}`} className="hover">
+          <li key={i + 1} className={`nav `}>
+            <Link
+              onClick={() => setActive(item)}
+              to={`/${item.toLowerCase()}`}
+              className={`hover ${active === item && "text-secondary-200"}`}
+            >
               {item}
             </Link>
           </li>
