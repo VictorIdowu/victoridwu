d2671eae0a039e7d1f36a866c949efc261564344
VictorIdowu <idsonvictor@gmail.com>
2024-12-28 17:00:11 +0100

    socials fix

--- src/Components/Socials.jsx
+++ src/Components/Socials.jsx
@@ -2,33 +2,29 @@ import { AiFillGithub } from "react-icons/ai";
 import { BiLogoLinkedin } from "react-icons/bi";
 import { Icon } from "@iconify/react";
 
+const socials = [
+  { icon: <AiFillGithub />, url: "https://github.com/VictorIdowu" },
+  { icon: <BiLogoLinkedin />, url: "https://linkedin.com/in/vctoridowu" },
+  {
+    icon: <Icon icon="ri:twitter-x-fill" className="scale-75" />,
+    url: "https://twitter.com/VictorIdowu_",
+  },
+];
+
 const Socials = () => {
   return (
     <aside data-aos="fade-up" className={`social ease-in duration-700`}>
-      <a
-        className="socials"
-        rel="noopener noreferrer"
-        href="https://github.com/VictorIdowu"
-        target="_blank"
-      >
-        <AiFillGithub />
-      </a>
-      <a
-        className="socials"
-        rel="noopener noreferrer"
-        href="https://linkedin.com/in/vctoridowu"
-        target="_blank"
-      >
-        <BiLogoLinkedin />
-      </a>
-      <a
-        className="socials"
-        rel="noopener noreferrer"
-        href="https://twitter.com/VictorIdowu_"
-        target="_blank"
-      >
-        <Icon icon="ri:twitter-x-fill" className="scale-75" />
-      </a>
+      {socials.map((social, i) => (
+        <a
+          key={i}
+          className="md:mb-4 hover:text-secondary-200 transition-colors duration-300"
+          rel="noopener noreferrer"
+          href={social.url}
+          target="_blank"
+        >
+          {social.icon}
+        </a>
+      ))}
     </aside>
   );
 };
